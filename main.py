a5a2e00fc1395777947906b99d50deaba95eb32e
Kamohsnap_AWS <kamohsnapaws@gmail.com>
2025-02-02 16:57:05 -0500

    updated main.py with a functioning black screen that stays until exit prompted

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-def startup():
-    # This hides the welcome message output by pygame
-    import os
-    os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-    # This allows us to use code from the open-source 
-    # pygame library throughout this file
-    import pygame    
+# This hides the welcome message output by pygame
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+# This allows us to use code from the open-source 
+# pygame library throughout this file
+import pygame    
 # Asteroid game constants    
 from constants import *
 
 
 def main():
+    pygame.init()
+    
     print(f"Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Infinite loop
+    while True:
+        # Allows for exiting the infinite loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
 if __name__ == "__main__":
-    startup()
     main()
