f5a12842e096fe888fc5733f4b05e88f4a7882b8
Kamohsnap_AWS <kamohsnapaws@gmail.com>
2025-02-02 19:21:09 -0500

    updated main.py limiting the fps to reduce resources used

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ from constants import *
 
 def main():
     pygame.init()
+    game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
     
     print(f"Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     # Infinite loop
     while True:
@@ -23,8 +24,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+        
+        game_screen.fill("black")
         pygame.display.flip()
+        
+        # Limits framerate to 60 FPS
+        dt = (game_clock.tick(60) / 1000)
 
 
 if __name__ == "__main__":
